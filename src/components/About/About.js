2c0ca5204bcb8066fb53b6ea9c6b0f970bc8c4d4
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-04-05 23:47:52 +0600

    sixth

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,14 +3,11 @@ import React from 'react'
 function About() {
   return (
     <div>
-        <h2>Laptop For Start <span>Coding</span></h2>
-          <h2>Best <span>Laptop</span> For Starting </h2>
+        <h2>Camera For Start <span>Photography</span></h2>
+          <h2>Best <span>Camera</span> For Starting </h2>
           
         <p>
-          Hey are you wanna start coding?
-          But you have low budget for starting your journey.
-          Don't worry we are here for help you. We have some collection that maybe match with you budget.
-          Happy learning.
+         <strong>Do professional photographers use DSLR cameras?</strong> The cameras professional photographers mostly use are DSLRs. Their design and function comes from film cameras. The higher-end models have a full-frame sensor and the lenses are interchangeable on DSLRs and these lenses range in quality.
         </p>
     </div>
   )
